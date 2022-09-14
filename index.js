c7dd5f491e585bf560cf594c4d7fa498cc8255cb
hayaa123 <hayalawansah@gmail.com>
2022-09-14 15:13:00 +0300

    added projectiles

--- index.js
+++ index.js
@@ -76,12 +76,91 @@ class Projectile {
     }
     draw (){
         c.beginPath()
-        c.arc()
+        c.arc(this.position.x,this.position.y,this.raduis,0, Math.PI*2)
+        c.fillStyle = "red"
+        c.fill()
+        c.closePath()
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+class Invader {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+         }
+
+        const image = new Image()
+        image.src = "./assets/invader.png"
+        image.onload = () => {
+
+            let scale = 1
+            this.image = image
+            this.width = image.width *scale
+            this.height = image.height *scale
+
+            this.position = {
+                x: canvus.width/2 -this.width/2,
+                y: canvus.height /2
+            }
+        } // this function run when the image is loaded
+
+
+    }
+    draw() {
+
+        
+        // c.fillStyle = "red",
+        // c.fillRect(this.position.x,this.position.y,this.width,this.height)
+        if (this.image) { // we put this if statment because when we do onload this will run before the attributes get added 
+            c.drawImage(
+                this.image,
+                this.position.x, 
+                this.position.y, 
+                this.width, 
+                this.height
+                )
+        }
+    }
+    update(){
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+        }
+      
+    }
+}
+
+class Grid {
+    constructor (){
+       this.position = {
+        x:0 , 
+        y:0
+       } 
+       this.velocity = {
+        x:0,
+        y:0
+       }
+       this.invaders =[
+        new Invader()
+       ]
     }
 }
 
 
 let player1 = new Player()
+let projectiles = []
+let grid = [new Grid()]
+
+// let invader = 
+
 player1.draw()
 
 // when we try to render the player in this way the image will not appear 
@@ -111,6 +190,18 @@ function animate() { // this do somthing like infinite loopp but without affecti
     c.fillStyle = "black"
     c.fillRect(0, 0, canvus.width, canvus.height)
     player1.update()
+
+    projectiles.forEach((projectile,index) =>{
+        if (projectile.position.y+ projectile.raduis<=0){ // this called garbage collection so when projectile go out of the screen we will delete it 
+            setTimeout(()=>{
+                projectiles.splice(index,1)
+            },0)
+        }else{
+            projectile.update() 
+ 
+        }
+    })
+    
 
     if (keys.ArrowLeft.pressed && player1.position.x>=0){
         player1.velocity.x = -5 
@@ -148,7 +239,8 @@ addEventListener("keydown",(e)=>{
             keys.ArrowRight.pressed = true
             break
         case " ":
-            console.log('space');
+            projectiles.push(new Projectile({x:player1.position.x+ 0.5*player1.width ,y:player1.position.y},{x:0,y:-5}))
+
             break
     }
 })
